90907fe40d1d8caed0f7144b93607c5e25325cef
dbBurzum <khalidsr@code.edu.az>
2022-05-28 20:54:57 +0400

    almost done

--- assets/apps/app.js
+++ assets/apps/app.js
@@ -5,7 +5,8 @@ let surName = document.getElementById("surNameInput");
 let nickName = document.getElementById("nickName");
 let userInfo = document.getElementById("userInfo");
 let addUser = document.getElementById("addUser");
-
+let form = document.getElementById("form");
+let img="./assets/avatars/avatar.jpg";
 addUser.onclick=function(){
     if (
         name.value == "" ||
@@ -15,74 +16,32 @@ addUser.onclick=function(){
         ){
             alert("Fill all the blanks and upload image again");        
         }  
-        else if(name.value != "" &&
-        surName.value != "" &&
-        nickName.value != "" &&
-        userInfo.value != ""){
+        else{
             addNewUser();
+            form.reset();
 
         }
 }
-
 file.addEventListener("change", (e) => {
-  if (
-    name.value == "" ||
-    surName.value == "" ||
-    nickName.value == "" ||
-    userInfo.value == ""
-  ) {
-    file.value = "";
-    alert("Fill all the blanks and upload image again");
-  } else {
-    uploadImage(e.target.files);
-    file.value = "";
+  for (const file of e.target.files) {
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = function (image) {
+      img=image.target.result;
+    };
   }
 });
 
 function addNewUser(){
     let card = `
     <div class="card m-2" style="width: 18rem;">
-    <img src="assets/avatars/avatar.jpg" class="card-img-top" alt="...">
+    <img src="${img}" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">${nickName.value}</h5>
       <p class="card-text">${userInfo.value}</p>
       <p class="card-text">${name.value + " " + surName.value}</p>
     </div>
   </div>
-    `;
+    `
 userArea.innerHTML += card;
-
-(name.value = ""),
-(surName.value = ""),
-(nickName.value = ""),
-(userInfo.value = ""),
-(file.value = "");
-
-}
-function uploadImage(item) {
-  for (const file of item) {
-    let reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onloadend = function (image) {
-      console.log(file.name + " file loaded");
-      let card = `
-                <div class="card m-2" style="width: 18rem;">
-                <img src="${
-                  image.target.result
-                }" class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">${nickName.value}</h5>
-                  <p class="card-text">${userInfo.value}</p>
-                  <p class="card-text">${name.value + " " + surName.value}</p>
-                </div>
-              </div>
-                `;
-      userArea.innerHTML += card;
-      (name.value = ""),
-        (surName.value = ""),
-        (nickName.value = ""),
-        (userInfo.value = ""),
-        (file.value = "");
-    };
-  }
 }
